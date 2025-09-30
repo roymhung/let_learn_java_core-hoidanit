2a327a7c7f2c201cad0780fe0b7c81a35d704522
royhung <lemanhhung1555@gmail.com>
2025-09-30 22:19:56 +0700

    first commit

---
+++ src/main/java/com/example/java_core_hoidanit/JavaCoreHoidanitApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_core_hoidanit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreHoidanitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCoreHoidanitApplication.class, args);
+	}
+
+}
