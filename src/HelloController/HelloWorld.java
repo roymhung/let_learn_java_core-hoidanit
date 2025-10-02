7183b76eb44fa1bb951a2ab55ca5d70f3a2f7989
royhung <lemanhhung1555@gmail.com>
2025-10-02 11:08:00 +0700

    test variable

---
+++ src/HelloController/HelloWorld.java
@@ -0,0 +1,9 @@
+package HelloController;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 26;
+        String name = "roy hung";
+        System.out.println("my name is : " + name + "my age: " + age);
+    }
+}
